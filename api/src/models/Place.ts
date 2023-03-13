4afcc9b067fa96a2d5fe045ff3adf2fadc252fc5
Akash <samantray745@gmail.com>
2023-03-13 21:38:03 +0100

    Reservation Model

---
+++ api/src/models/Place.ts
@@ -0,0 +1,29 @@
+import mongoose, { Document } from "mongoose";
+
+export type PlaceDocument = Document & {
+  owner: string;
+  title: string;
+  address: string;
+  photos: [string];
+  description: string;
+  perks: [string];
+  extraInfo: string;
+  checkIn: boolean;
+  checkOut: boolean;
+  maxGuests: number;
+  price: number;
+};
+const PlaceSchema = new mongoose.Schema({
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  title: { type: String },
+  address: { type: String },
+  photos: { type: [String] },
+  descriptions: { type: String },
+  perks: { type: [String] },
+  extraInfo: { type: String },
+  checkIn: { type: Boolean },
+  checkOut: { type: Boolean },
+  maxGuests: { type: Number },
+  price: { type: Number },
+});
+export default mongoose.model<PlaceDocument>("Place", PlaceSchema);
