569dcf1570d469514288e8af8d82205510b493cf
Akash <samantray745@gmail.com>
2023-03-14 11:32:10 +0100

    Reservation Routes and Controller

---
+++ api/src/controllers/reservation.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import Reservation from "../models/Reservation";
+import ReservationServices from "../services/ReservationServices";
+
+export const createReservationController = async (req: Request, res: Response) => {
+  try {
+    const newReservation = new Reservation({
+      userId: req.params.userId,
+      place: req.body.place,
+      price: req.body.price,
+    });
+    const reservation = await ReservationServices.createReservation(newReservation);
+    res.json(reservation);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getReservationByUserIdController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const foundReservation = await ReservationServices.getReservationById(req.params.userId);
+    res.json(foundReservation);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteReservationByIdController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const foundReservation = await ReservationServices.deleteReservationById(req.params.id);
+    res.json(foundReservation);
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ api/src/routes/reservation.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { createReservationController, getReservationByUserIdController,deleteReservationByIdController } from "../controllers/reservation";
+import passport from "passport"
+
+const router = Router();
+
+router.post(
+    "/:userId",
+    //passport.authenticate("jwt", { session: false }),
+    createReservationController
+  );
+  router.get(
+    "/:userId",
+    //passport.authenticate("jwt", { session: false }),
+    getReservationByUserIdController
+  );
+  router.delete(
+    "/:id",
+   // passport.authenticate("jwt", { session: false }),
+    deleteReservationByIdController
+  );
