cb02c07fdc42be0d81c48bc73ac1c1660f681ff4
Yasser Shalash <yasservanshalash@gmail.com>
2023-03-15 14:46:44 +0100

    added redux for users and places

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Box } from '@mui/material';
@@ -7,7 +7,17 @@ import LandingPage from './pages/LandingPage';
 import Footer from './components/footer/Footer';
 import { Route, Routes } from 'react-router-dom';
 import PropertiesPage from './pages/PropertiesPage';
+import { AppDispatch, RootState } from './redux/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPlacesData } from './redux/thunk/placesThunk';
 function App() {
+  const places = useSelector((state: RootState) => state.places.places)
+  const dispatchThunk = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatchThunk(fetchPlacesData);
+  }, [])
+  console.log(places)
   return (
     <Box className="App" sx={{display: "flex", flexDirection:"column"}}>
       <NavBar />
