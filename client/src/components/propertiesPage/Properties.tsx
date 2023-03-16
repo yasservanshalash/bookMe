2054b684875d61d1f722c26bf5eca4addaca23d9
Yasser Shalash <yasservanshalash@gmail.com>
2023-03-16 23:13:31 +0100

    minor change

--- client/src/components/propertiesPage/Properties.tsx
+++ client/src/components/propertiesPage/Properties.tsx
@@ -12,7 +12,7 @@ const Properties = () => {
   return (
     <Box sx={{width: "70%", height: "90vh", overflowY: "scroll", margin: "0 auto"}}>
         <Box>
-        <Typography>{searchTerm === "" ? "" : `${properties.filter((property) => property.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()) || property.address.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())).length } properties found`}</Typography>
+        <Typography variant='h6' sx={{fontWeight: "bold", m:2}}>{searchTerm === "" ? "" : `${properties.filter((property) => property.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()) || property.address.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())).length } properties found`}</Typography>
         <Box sx={{display: "flex", flexDirection: "column", gap: 5}}>
         {
           properties.filter((property) => property.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()) || property.address.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())).map((property) => {
