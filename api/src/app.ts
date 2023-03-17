85eff17bccd9938ad36cdb7a9ce5180ccfb453e9
hyacinth-bruno <mbachubruno.chidozie@gmail.com>
2023-03-17 13:44:55 +0100

    GoogleLogIn Commit
    Code for google login

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,11 @@
 //server here
 import Express from "Express";
 import cors from "cors";
+
+
+import  passport  from 'passport';
+import {googleStrategy} from "./config/passport"
+
 //routes
 import userRouter from "./routes/users";
 import placeRouter from "./routes/place";
@@ -13,5 +18,13 @@ app.use(cors());
 app.use("/users", userRouter);
 app.use("/places", placeRouter);
 app.use("/wishlist", wishListRouter);
+<<<<<<< Updated upstream
 app.use("/review", reviewRouter);
+||||||| Stash base
+=======
+
+app.use(passport.initialize());
+passport.use(googleStrategy)
+
+>>>>>>> Stashed changes
 export default app;
