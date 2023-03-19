a705cd8d21ddab46d75534bd39cfbe9ba2b008f5
Yasser Shalash <yasservanshalash@gmail.com>
2023-03-19 11:41:19 +0100

    added loved places in landing page

--- client/src/components/landingPage/lovedPlaces/LovedPlaces.tsx
+++ client/src/components/landingPage/lovedPlaces/LovedPlaces.tsx
@@ -1,17 +1,23 @@
 import { Box, Typography } from '@mui/material'
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../../redux/store'
 import LovedPlace from './LovedPlace'
 
 const LovedPlaces = () => {
+  const places = useSelector((state: RootState) => state.places.places)
   return (
     <Box sx={{width: "90%", margin: "0 auto 50px auto"}}>
         <Typography variant='h6' sx={{textAlign: "left", my: 4}}>Places loved by guests</Typography>
         <Box sx={{display: "flex", justifyContent: "center", gap: 2 }}>
-        <LovedPlace name={"The London"} city={"Amsterdam"}  price={170} rating={9.2} image={"https://images.squarespace-cdn.com/content/v1/6165069dbcf00c0b136b53a3/1634013748621-7ODMK8QTWESQEK66II8G/tl-home-hero.jpg?format=2500w"} />
-        <LovedPlace name={"The London"} city={"Amsterdam"}  price={170} rating={9.2} image={"https://images.squarespace-cdn.com/content/v1/6165069dbcf00c0b136b53a3/1634013748621-7ODMK8QTWESQEK66II8G/tl-home-hero.jpg?format=2500w"} />
-        <LovedPlace name={"The London"} city={"Amsterdam"}  price={170} rating={9.2} image={"https://images.squarespace-cdn.com/content/v1/6165069dbcf00c0b136b53a3/1634013748621-7ODMK8QTWESQEK66II8G/tl-home-hero.jpg?format=2500w"} />
-        <LovedPlace name={"The London"} city={"Amsterdam"}  price={170} rating={9.2} image={"https://images.squarespace-cdn.com/content/v1/6165069dbcf00c0b136b53a3/1634013748621-7ODMK8QTWESQEK66II8G/tl-home-hero.jpg?format=2500w"} />
-        <LovedPlace name={"The London"} city={"Amsterdam"}  price={170} rating={9.2} image={"https://images.squarespace-cdn.com/content/v1/6165069dbcf00c0b136b53a3/1634013748621-7ODMK8QTWESQEK66II8G/tl-home-hero.jpg?format=2500w"} />
+        {
+          places.slice(0,5).map((place) => {
+            return (
+              <LovedPlace name={place.title} city={place.place}  price={place.price} rating={9.2} image={place.photos[0]} />
+
+            )
+          })
+        }
         </Box>
     </Box>
   )
